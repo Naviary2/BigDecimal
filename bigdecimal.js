70b69cf26010b2ca95951fdc38cbff155159c7ff
Naviary2 <infinitechess.org@gmail.com>
2024-03-31 23:16:39 -0600

    Added a changelog, and metadata at the top of bigdecimal.js

--- bigdecimal.js
+++ bigdecimal.js
@@ -1,4 +1,30 @@
-"use strict";
+
+/**
+ * bigdecimal.js v0.1.0 Beta
+ * High performance arbitrary-precision decimal type of Javascript.
+ * https://github.com/Naviary2/BigDecimal
+ * Copyright (c) 2024 Naviary (www.InfiniteChess.org) <infinitechess.org@gmail.com>
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 
 /**
  * TODO:
@@ -27,6 +53,8 @@
  * - Finish writing all remaining arithmetic methods of MathBigDec!
  * 
  */
+
+"use strict";
 
 // Useful Number constants
 const LOG_TWO = Math.log(2);
