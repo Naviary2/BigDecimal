94de4687168291e67684c7f7461d992860261cad
Naviary2 <infinitechess.org@gmail.com>
2025-12-04 05:52:15 -0700

    Better var names

--- benchmarks/index.ts
+++ benchmarks/index.ts
@@ -32,35 +32,39 @@ const n_one = FromNumber(1, BINARY_PRECISION);
 const n_three = FromNumber(3, BINARY_PRECISION);
 
 // Prepare @naviary/bigdecimal inputs
-const naviary_A = divide_floating(n_one, n_three, BINARY_PRECISION);
-const naviary_B = sqrt(FromNumber(2, BINARY_PRECISION), BINARY_PRECISION);
+const bigdecimal_A = divide_floating(n_one, n_three, BINARY_PRECISION);
+const bigdecimal_B = sqrt(FromNumber(2, BINARY_PRECISION), BINARY_PRECISION);
 
 // Prepare decimal.js inputs
 const decimal_A = new Decimal(1).div(3);
 const decimal_B = new Decimal(2).sqrt();
 
 // Prepare bignumber.js inputs
-const bn_A = new BigNumber(1).div(3);
-const bn_B = new BigNumber(2).sqrt();
+const bignumber_A = new BigNumber(1).div(3);
+const bignumber_B = new BigNumber(2).sqrt();
 
 // BENCHMARKS
 
 group('Addition (0.33 + 1.41)', () => {
 	bench('decimal.js', () => decimal_A.plus(decimal_B));
-	bench('bignumber.js', () => bn_A.plus(bn_B));
-	bench('@naviary/bigdecimal', () => add(naviary_A, naviary_B));
+	bench('bignumber.js', () => bignumber_A.plus(bignumber_B));
+	bench('@naviary/bigdecimal', () => add(bigdecimal_A, bigdecimal_B));
 });
 
 group('Multiplication (0.33 * 1.41)', () => {
 	bench('decimal.js', () => decimal_A.times(decimal_B));
-	bench('bignumber.js', () => bn_A.multipliedBy(bn_B));
-	bench('@naviary/bigdecimal', () => multiply_floating(naviary_A, naviary_B, BINARY_PRECISION));
+	bench('bignumber.js', () => bignumber_A.multipliedBy(bignumber_B));
+	bench('@naviary/bigdecimal', () =>
+		multiply_floating(bigdecimal_A, bigdecimal_B, BINARY_PRECISION),
+	);
 });
 
 group('Division (0.33 / 1.41)', () => {
 	bench('decimal.js', () => decimal_A.div(decimal_B));
-	bench('bignumber.js', () => bn_A.div(bn_B));
-	bench('@naviary/bigdecimal', () => divide_floating(naviary_A, naviary_B, BINARY_PRECISION));
+	bench('bignumber.js', () => bignumber_A.div(bignumber_B));
+	bench('@naviary/bigdecimal', () =>
+		divide_floating(bigdecimal_A, bigdecimal_B, BINARY_PRECISION),
+	);
 });
 
 group('Feature: Fixed-Point Multiplication', () => {
@@ -69,10 +73,10 @@ group('Feature: Fixed-Point Multiplication', () => {
 		decimal_A.times(decimal_B).toDecimalPlaces(DECIMAL_PRECISION),
 	);
 	bench('bignumber.js (simulated)', () =>
-		bn_A.multipliedBy(bn_B).decimalPlaces(DECIMAL_PRECISION),
+		bignumber_A.multipliedBy(bignumber_B).decimalPlaces(DECIMAL_PRECISION),
 	);
 	// My native fixed-point method
-	bench('@naviary/bigdecimal', () => multiply_fixed(naviary_A, naviary_B));
+	bench('@naviary/bigdecimal', () => multiply_fixed(bigdecimal_A, bigdecimal_B));
 });
 
 await run();
